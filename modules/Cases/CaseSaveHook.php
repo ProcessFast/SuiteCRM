155f80b1aa18faeb4857f5b508d1b95fc6d19d46
Er.Kalpesh <er.kalpesh.thakkar@gmail.com>
2017-09-16 17:04:51 +0530

    1356: Cases Module - Cases.Description Length Not Checked and Truncated Before Insert Statement

---
+++ modules/Cases/CaseSaveHook.php
@@ -0,0 +1,22 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+class CaseSaveHook
+{
+    var $module = 'Cases';
+    private $slug_size = 65530;
+    
+    /**
+     * @param aCase $case
+     */
+    public function saveCase($bean, $event, $arguments)
+    {
+        $text = $bean->description;
+        if (!$text) {
+            return;
+        }
+        $bean->description = '';
+        if (strlen($text) > $this->slug_size) {
+            $bean->description = substr($text, 0, $this->slug_size) . '...';
+        }                
+    }
+}
