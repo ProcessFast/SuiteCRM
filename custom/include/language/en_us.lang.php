a84f5802739643373a1520d85800a85e1df69be4
Brad Stancel <brad@processfast.com>
2018-02-23 18:28:11 -0500

    Added SQL Writer to custom list of Supplier Types

--- custom/include/language/en_us.lang.php
+++ custom/include/language/en_us.lang.php
@@ -24,23 +24,6 @@ $GLOBALS['app_list_strings']['contract_type_list']=array (
   'monthly' => 'Monthly',
   'annual' => 'Annual',
 );
-$GLOBALS['app_list_strings']['supplier_type_list']=array (
-  '' => '',
-  'supplier_functional_testing' => 'Testing / QA',
-  'supplier_channel_partner_master_agent' => 'Channel Partner - Master Agent',
-  'supplier_channel_partner_direct_supplier' => 'Channel Partner - Direct',
-  'supplier_c_sharp_developer' => 'Developer - C#',
-  'supplier_php_developer' => 'Developer - PHP',
-  'supplier_functional_consultant' => 'Functional Consultant',
-  'supplier_linux' => 'Linux / SysAdmin',
-  'supplier_graphic_designer' => 'Graphic Designer',
-  'supplier_product_designer' => 'Product Designer',
-  'supplier_content_writer' => 'Content Writer / Provider',
-  'supplier_project_manager' => 'Project Manager',
-  'supplier_seo_provider' => 'SEO',
-  'supplier_video_producer' => 'Video Creator / Editor',
-);
-
 $GLOBALS['app_list_strings']['industry_dom']=array (
   '' => '',
   'Apparel' => 'Apparel',
@@ -159,4 +142,21 @@ $GLOBALS['app_list_strings']['account_type_dom']=array (
   'Reseller' => 'Reseller',
   'supplier' => 'Supplier',
   'Other' => 'Other',
+);
+$GLOBALS['app_list_strings']['supplier_type_list']=array (
+  '' => '',
+  'supplier_functional_testing' => 'Testing / QA',
+  'supplier_channel_partner_master_agent' => 'Channel Partner - Master Agent',
+  'supplier_channel_partner_direct_supplier' => 'Channel Partner - Direct',
+  'supplier_c_sharp_developer' => 'Developer - C#',
+  'supplier_php_developer' => 'Developer - PHP',
+  'supplier_functional_consultant' => 'Functional Consultant',
+  'supplier_linux' => 'Linux / SysAdmin',
+  'supplier_graphic_designer' => 'Graphic Designer',
+  'supplier_product_designer' => 'Product Designer',
+  'supplier_content_writer' => 'Content Writer / Provider',
+  'supplier_project_manager' => 'Project Manager',
+  'supplier_sql_writer' => 'SQL Writer',
+  'supplier_seo_provider' => 'SEO',
+  'supplier_video_producer' => 'Video Creator / Editor',
 );
